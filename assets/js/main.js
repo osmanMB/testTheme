f70a4b08cf022be2f5418ff9cae185e3589cd689
osman792 <osmann792@gmail.com>
2020-05-12 01:07:24 +0200

    style updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,17 @@ $(document).ready(function() {
 		}
 	});
 
+	$('[placeholder]').focus(function () {
+
+		$(this).attr('data-text', $(this).attr('placeholder'));
+
+		$(this).attr('placeholder', '');
+
+	}).blur(function () {
+
+		$(this).attr('placeholder', $(this).attr('data-text'));
+
+	});
+
+
 });
